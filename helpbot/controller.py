d0be3ce81c2d2be0c25430d20b629b28b14673ee
AdrienMorvan <adrien.morvan@photobox.com>
2017-06-30 23:53:46 +0200

    Full question/answer ready

--- helpbot/controller.py
+++ helpbot/controller.py
@@ -8,12 +8,14 @@ from werkzeug.wrappers import Response
 from functools import wraps
 from config import Config as config
 from . import LOG, sentry, login_manager
-import os
+import hashlib
 import requests
 from . import app
+from redis import StrictRedis
+import cPickle
 
 api_v1 = Blueprint('helpbot', __name__)
-
+r = StrictRedis()
 #
 # API calls handling tools
 #
@@ -28,16 +30,57 @@ def format_response(response, status_code=200):
 @api_v1.route("/helpbot", methods=['POST', 'GET'])
 def main():
 
+    # Get the data
     question = request.form.get('text')
     name = request.form.get('user_name')
+    ticket = hashlib.md5(name + question + config.get("salt")).hexdigest()
 
-    first_sentence = "Hello {}, I took your question to the admins.".format(name)
-    second_sentence = "'{}' is a really nice question !".format(question)
+    r.set(ticket, cPickle.dumps((name, question)))
 
-    requests.post('https://hooks.slack.com/services/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',
-                  data=json.dumps({"text": "A new question from {} : {}".format(name, question)}))
+    first_sentence = "Hello {},\n I took your question to the admins.\n".format(name)
+    second_sentence = "Here is you ticket number {}.\n An admin will answer your question soon.".format(ticket)
+
+    # Post data to the room
+    post_on_channel = {"text": "A new question from {} : {}\n. The ticket number is {}".\
+                       format(name, question, ticket),
+                       "channel": "#test_helpbot", "link_names": 1, "username": "help-bot"}
+    room_post_url = config.get('room_post_url')
+
+    requests.post(room_post_url,
+                  data=json.dumps(post_on_channel))
 
     code, response = 200, {"text": first_sentence + second_sentence}
+
+    return jsonify(response), code
+
+
+@api_v1.route("/answer", methods=['POST', 'GET'])
+def answer():
+
+    req = request.form.get('text')
+    name = request.form.get('user_name')
+
+    params = req.split(" ")
+    ticket = params[0]
+    answer = ""
+    for word in params[1:]:
+        answer += " " + word
+
+    data = r.get(ticket)
+    if data is not None:
+        question_name, question = cPickle.loads(data)
+    else:
+        raise Exception
+
+    first_sentence = "Hello everyone,\n {} answer the question of {}.\n".format(name, question_name)
+    second_sentence = "Here is the question : {}\n".format(question)
+    third_sentence = "Here is the answer : {}\n".format(answer)
+
+    answer_room_url = config.get("answer_post_url")
+    requests.post(answer_room_url,
+                  data=json.dumps({"text": first_sentence + second_sentence + third_sentence}))
+
+    code, response = 200, {"text": first_sentence + second_sentence + third_sentence}
 
     return jsonify(response), code
 
